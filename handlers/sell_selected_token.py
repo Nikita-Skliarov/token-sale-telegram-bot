6d35eec49d2ab7a00390436fdc4f38ac8c449b52
Nikita Skliarov <0357026@student.rocvantwente.nl>
2024-06-10 09:05:29 +0200

    Edit text

--- handlers/sell_selected_token.py
+++ handlers/sell_selected_token.py
@@ -16,7 +16,7 @@ async def Sell_selected_token(update: Update, context: ContextTypes.DEFAULT_TYPE
         selected_token = context.user_data.get("selected_token")
         message_text = (
         f"Вы выбрали *{selected_token}* как токен, который хотите продать. \n"
-        f"В скором времени с вами свяжется поддержка, чтобы выяснить реквизиты для получения, количество и произвести продажу."
+        f"В скором времени с Вами свяжется поддержка, чтобы выяснить реквизиты для получения, количество и произвести продажу."
         )
         context.user_data["status"] = message_text
         await update.callback_query.message.reply_text(message_text, reply_markup=sell_selected_token_keyboard_m, parse_mode="Markdown")
--- handlers/start.py
+++ handlers/start.py
@@ -6,7 +6,7 @@ async def Start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_text = (
         "Приветствую!\n"
         "С помощью этого бота Вы сможете продать токены и криптовалюту.\n"
-        "Чтобы продолжить, нажми на кнопку внизу и выбери следующие варианты."
+        "Чтобы продолжить, нажмите на кнопку внизу и выбери следующие варианты."
     )
     if update.callback_query:
         query = update.callback_query
